4a492555ab1535c226e68b0ad052962a32c209c1
lizabautista <31431795+lizabautista@users.noreply.github.com>
2019-04-07 00:57:33 -0700

    Add files via upload

---
+++ sections/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+		<head>
+			<title>
+				<?=$title?>
+			</title>
+			<script src='./js/bootstrap.js'></script>
+			<script src='./js/popper.js' ></script>
+			<link href="css/bootstrap.css" rel="stylesheet">
+	
